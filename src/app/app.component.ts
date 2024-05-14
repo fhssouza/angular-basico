056b75caab5dd1d597a01cfe87873282dc19ff71
fhssouza <fhssouza@gmail.com>
2024-05-14 17:27:42 -0300

    primeiro componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {Componente01Component} from "./componente01/componente01.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, Componente01Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
