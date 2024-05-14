47f8a49dc54a53802b4ac684c02f8f159376b734
fhssouza <fhssouza@gmail.com>
2024-05-14 17:45:11 -0300

    Event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {Componente01Component} from "./componente01/componente01.component";
+import {Componente02Component} from "./componente02/componente02.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Componente01Component],
+  imports: [CommonModule, RouterOutlet, Componente01Component, Componente02Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
