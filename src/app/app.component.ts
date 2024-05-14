3f5c2c89407bf819ab7ced2bc8d7d62925195041
fhssouza <fhssouza@gmail.com>
2024-05-14 18:04:42 -0300

    Property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {Componente01Component} from "./componente01/componente01.component";
 import {Componente02Component} from "./componente02/componente02.component";
+import {Componente03Component} from "./componente03/componente03.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Componente01Component, Componente02Component],
+  imports: [CommonModule, RouterOutlet, Componente01Component, Componente02Component, Componente03Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/componente03/componente03.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente03',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente03.component.html',
+  styleUrl: './componente03.component.css'
+})
+export class Componente03Component {
+
+  //Variavél de imagem
+  imagem: string = 'assets/dia.jpg';
+
+  //Função para alterar a imagem
+  alterarImagem(){
+    if(this.imagem === 'assets/dia.jpg'){
+      this.imagem = 'assets/noite.jpg';
+    }else{
+      this.imagem = 'assets/dia.jpg';
+    }
+  }
+
+
+}
